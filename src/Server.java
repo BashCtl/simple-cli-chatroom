02eff638f563480de8f8ff9f5ca3c58d740aa7ae
darko <ki14.skakun@gmail.com>
2023-12-26 16:10:23 +0200

    server creation in progress

---
+++ src/Server.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server implements Runnable {
+
+
+    private ArrayList<ConnectionHandler> connections;
+
+    @Override
+    public void run() {
+        try {
+            ServerSocket server = new ServerSocket(7777);
+            Socket client = server.accept();
+            ConnectionHandler handler = new ConnectionHandler(client);
+            connections.add(handler);
+        } catch (IOException e) {
+            // TODO: handle
+        }
+    }
+
+    class ConnectionHandler implements Runnable {
+
+        private Socket client;
+        private BufferedReader in;
+        private PrintWriter out;
+        private String nickname;
+
+        public ConnectionHandler(Socket client) {
+            this.client = client;
+        }
+
+        @Override
+        public void run() {
+            try {
+                out = new PrintWriter(client.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                out.println("Please enter a nickname: ");
+                nickname = in.readLine();
+                System.out.println(nickname + " connected!");
+            } catch (IOException e) {
+                // TODO: handler
+            }
+        }
+
+        public void sendMessage(String message){
+
+        }
+    }
+}
